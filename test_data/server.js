112fc3043051ab46f2fe2b1ba880aea41dcae0b4
Shoeb Ilyas <73349037+shoebilyas123@users.noreply.github.com>
2025-11-14 09:22:04 +0530

    feat: file path and arguments support (#1)

--- test_data/server.js
+++ test_data/server.js
@@ -6,5 +6,11 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(4000, () => {
+  const arg1 = process.argv[2];
+  const arg2 = process.argv[3];
+
+  console.log("First argument:", arg1);
+  console.log("Second argument:", arg2);
+
   console.log("Server running at http://localhost:4000/");
 });
